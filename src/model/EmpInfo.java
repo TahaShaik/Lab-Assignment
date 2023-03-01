4e7c80443618c0070b1a783719ebe7573a4e3118
ShaikTaha1 <>
2023-03-02 01:51:13 +0530

    First Commit

---
+++ src/model/EmpInfo.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+
+public class EmpInfo {
+    
+    private String name;
+    private int employeeId;
+    private int age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String phoneNumber;
+    private String email;
+    private ImageIcon Photo;
+
+    public ImageIcon getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(ImageIcon Photo) {
+        this.Photo = Photo;
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return name;
+    }
+}
